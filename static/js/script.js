2fbfcee80398d8f412ce1331772db08f140ae6ab
Rakhi Purwar <42908255+rakhiPurwar@users.noreply.github.com>
2021-07-27 07:31:14 +0530

    Delete static directory

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-function onSignIn(googleUser)
-{
-	var profile=googleUser.getBasicProfile();
-	$(".g-sigin2").cc("display","none");
-	$(".data").css("display","block");
-	$("#pic").attr("scr",profile.getImageUrl());
-	$("email").text(profile.getEmail());
-}
-
